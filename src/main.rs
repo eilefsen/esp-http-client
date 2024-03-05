fccf7523477f86641e135a5050ed0112acdbe4c0
Emma Eilefsen Glenna <emma@eilefsen.net>
2024-03-05 17:51:56 +0100

    add wifi

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,66 @@
-fn main() {
-	_esp_boiler_plate();
-    log::info!("Hello, world!");
+use anyhow::{self, Result};
+use esp_idf_svc::eventloop::EspSystemEventLoop;
+use esp_idf_svc::hal::modem::Modem;
+use esp_idf_svc::hal::peripherals::Peripherals;
+use esp_idf_svc::nvs::EspDefaultNvsPartition;
+use esp_idf_svc::wifi::{ClientConfiguration, Configuration, EspWifi};
+
+#[toml_cfg::toml_config]
+pub struct Config {
+    #[default("Wokwi-GUEST")]
+    wifi_ssid: &'static str,
+    #[default("")]
+    wifi_psk: &'static str,
 }
 
-fn _esp_boiler_plate() {
+
+fn main() -> Result<()> {
+    _esp_setup()?;
+    log::info!("Hello, world!");
+
+    Ok(())
+}
+
+fn _esp_setup() -> Result<()> {
     // It is necessary to call this function once. Otherwise some patches to the runtime
     // implemented by esp-idf-sys might not link properly. See https://github.com/esp-rs/esp-idf-template/issues/71
     esp_idf_svc::sys::link_patches();
 
     // Bind the log crate to the ESP Logging facilities
     esp_idf_svc::log::EspLogger::initialize_default();
+    let peripherals = Peripherals::take().unwrap();
+    let _wifi = match _esp_wifi_setup(peripherals.modem) {
+        Ok(w) => {
+			log::info!("Wifi Successfully Connected!");
+			w
+		},
+        Err(err) => return Err(err),
+    };
 
+    Ok(())
+}
+
+fn _esp_wifi_setup(modem: Modem) -> Result<EspWifi<'static>> {
+    let sysloop = EspSystemEventLoop::take()?;
+    let nvs = EspDefaultNvsPartition::take()?;
+    let mut wifi = EspWifi::new(modem, sysloop, Some(nvs))?;
+
+    let cfg = Configuration::Client(ClientConfiguration {
+        ssid: heapless::String::try_from(CONFIG.wifi_ssid).unwrap(),
+        password: heapless::String::try_from(CONFIG.wifi_psk).unwrap(),
+        ..Default::default()
+    });
+
+    wifi.set_configuration(&cfg)?;
+    wifi.start()?;
+    wifi.connect()?;
+	// Confirm Wifi Connection
+    while !wifi.is_connected().unwrap() {
+        // Get and print connection configuration
+        let config = wifi.get_configuration().unwrap();
+        println!("Waiting for station {:?}", config);
+    }
+
+
+    Ok(wifi)
 }
